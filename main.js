828d2cbed9ef2f6e2586c47c02cc5f5dcff29fa5
arunadithyan <82517277+arunadithyan@users.noreply.github.com>
2022-09-21 20:24:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("Hello There");
+
+
+
+$(document).ready(function(){
+	$(".sub-btn").click(function(){
+		$(this).next(".sub-menu").slideToggle();
+	});
+});
+
+var menu = document.querySelector(".menu");
+var menuBtn = document.querySelector(".menu-btn");
+var closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click",() => {
+	menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click",() => {
+	menu.classList.remove("active");
+});
+
+
+
